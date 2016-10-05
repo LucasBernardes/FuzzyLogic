599b51683d1a4316e8ebfc5d75f727196d1466de
Dric0 <adriano_cardoso93@hotmail.com>
2016-10-05 10:32:17 -0300

    Ajustado valores de mercado

--- Fuzzylogic/src/fuzzylogic/Fuzzylogic.java
+++ Fuzzylogic/src/fuzzylogic/Fuzzylogic.java
@@ -4,20 +4,46 @@
  * and open the template in the editor.
  */
 package fuzzylogic;
+import net.sourceforge.jFuzzyLogic.*;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
 
 /**
  *
- * @author lucas
+ * @author dric0
  */
 public class Fuzzylogic {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Start");
-        System.out.println("Agora que o git ta rodando vai funfa o trab");
+    public static void main(String[] args) throws Exception {
+        // Load from 'FCL' file
+        String fileName = "/home/dric0/NetBeansProjects/FuzzyLogic/tipper.fcl";
+        FIS fis = FIS.load(fileName,true);
+
+        // Error while loading?
+        if( fis == null ) { 
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+        // Show 
+        FunctionBlock functionBlock = fis.getFunctionBlock(null);
+        JFuzzyChart.get().chart(functionBlock);
+
+        // Set inputs
+        fis.setVariable("service", 3);
+        fis.setVariable("food", 7);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart
+        Variable tip = functionBlock.getVariable("tip");
+        JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+
+        // Print ruleSet
+        System.out.println(fis);
     }
-    
 }
